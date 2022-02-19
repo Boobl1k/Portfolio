bcdf368744c36879a711d3e3a44d4cc46e492eb3
Mansur <ahmatowmansur@gmail.com>
2022-02-19 12:47:57 +0300

    Initial commit

---
+++ Portfolio/Controllers/QweController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers;
+
+public class QweController  : Controller
+{
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
